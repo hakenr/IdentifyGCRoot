3282a40bf7687ffd323b7a38abc47f126898f9fd
Robert Haken <haken@havit.cz>
2017-09-12 12:57:14 +0200

    NuGet package update

--- IdentifyGCRoot/Program.cs
+++ IdentifyGCRoot/Program.cs
@@ -11,26 +11,28 @@ namespace IdentifyGCRoot
 	{
 		static void Main(string[] args)
 		{
-			DataTarget dataTarget = DataTarget.LoadCrashDump(@"D:\Temp\_SampleDumps\2014-05-22 - DRAGON XWT+AUT (no issue) - w3wp\w3wp.DMP");
-			ClrRuntime runtime = dataTarget.CreateRuntime(@"C:\ProgramData\dbg\sym\mscordacwks_AMD64_AMD64_4.0.30319.18444.dll\52717F9A96b000\mscordacwks_AMD64_AMD64_4.0.30319.18444.dll");
-			ClrHeap heap = runtime.GetHeap();
-
-			foreach (ClrRoot root in heap.EnumerateRoots())
+			using (DataTarget dataTarget = DataTarget.LoadCrashDump(@"D:\Temp\2017-09-12 - BTF - BeTy DX Sessions\w3wp.DMP"))
 			{
-				if (root.Object == 0x00000000ffee0eb8) // adresa rootu, který chceme prozkoumat
+				ClrInfo runtimeInfo = dataTarget.ClrVersions[0];  // just using the first runtime
+				ClrRuntime runtime = runtimeInfo.CreateRuntime(@"C:\ProgramData\dbg\sym\mscordacwks_AMD64_AMD64_4.6.1087.00.dll\583E5E56990000\mscordacwks_AMD64_AMD64_4.6.1087.00.dll"); // .cordll -ve -u -l
+
+				foreach (ClrRoot root in runtime.Heap.EnumerateRoots())
 				{
-					Console.WriteLine(root.ToString());
-					Console.WriteLine($"\tAddress: {root.Address:x8}");
-					Console.WriteLine($"\tAppDomain: {root.AppDomain?.Name}");
-					Console.WriteLine($"\tIsInterior: {root.IsInterior}");
-					Console.WriteLine($"\tIsPinned: {root.IsPinned}");
-					Console.WriteLine($"\tIsPossibleFalsePositive: {root.IsPossibleFalsePositive}");
-					Console.WriteLine($"\tKind: {root.Kind}");
-					Console.WriteLine($"\tName: {root.Name}");
-					Console.WriteLine($"\tObject: {root.Object:x8}");
-					Console.WriteLine($"\tThread: {root.Thread}");
-					Console.WriteLine($"\tType: {root.Type}");
-					Console.WriteLine();
+					if (root.Object == 0x000000e9288f8f30) // address of root we want to examine
+					{
+						Console.WriteLine(root.ToString());
+						Console.WriteLine($"\tAddress: {root.Address:x8}");
+						Console.WriteLine($"\tAppDomain: {root.AppDomain?.Name}");
+						Console.WriteLine($"\tIsInterior: {root.IsInterior}");
+						Console.WriteLine($"\tIsPinned: {root.IsPinned}");
+						Console.WriteLine($"\tIsPossibleFalsePositive: {root.IsPossibleFalsePositive}");
+						Console.WriteLine($"\tKind: {root.Kind}");
+						Console.WriteLine($"\tName: {root.Name}");
+						Console.WriteLine($"\tObject: {root.Object:x8}");
+						Console.WriteLine($"\tThread: {root.Thread}");
+						Console.WriteLine($"\tType: {root.Type}");
+						Console.WriteLine();
+					};
 				}
 			}
 		}
